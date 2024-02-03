b0d32481a73e935f5b776bfd5d118aaec009504c
Blagoy <blagoy1234@gmail.com>
2024-02-03 10:54:56 +0200

    update

--- src/main/java/com/example/springdataintrodemo/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.springdataintrodemo.services;
-
-import com.example.springdataintrodemo.models.User;
-
-public interface UserService {
-    void registerUser(User user);
-
-    User getUserByUsername(String username);
-}
